0bcd8b2c7757c8abbcd1dfe74658544897818c3a
Calvin Julo <zcglook@hotmail.com>
2023-07-03 23:35:16 +0800

    Update st_helloworld.py

--- st_helloworld.py
+++ st_helloworld.py
@@ -1,7 +1,17 @@
 # coding=utf-8
 
 import streamlit as st
+import pandas as pd
 
 st.write('Hello World')
 
 st.success('Welcome to Streamlit')
+
+st.title("My Streamlit App")
+
+# Load data
+data_url = "https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/uber-raw-data-SEP14.csv.gz"
+data = pd.read_csv(data_url, compression="gzip")
+
+# Show data
+st.write(data)
