98556cb92d38fe2e7eca1a61428484d421455a5c
Calvin Julo <zcglook@hotmail.com>
2023-07-04 00:28:41 +0800

    Update st_helloworld.py

--- st_helloworld.py
+++ st_helloworld.py
@@ -2,24 +2,13 @@
 
 import streamlit as st
 import pymongo
+st.write('Start')
+pymongo.MongoClient("mongodb+srv://zcglook:OINbHLNnoSuvCjnud@cluster0.lfgvy2a.mongodb.net/?retryWrites=true&w=majority")
+st.write('Entry')
+try:
+    client.admin.command('ping')
+    st.write("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    st.write(e)
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return pymongo.MongoClient("mongodb+srv://zcglook:OINbHLNnoSuvCjnud@cluster0.lfgvy2a.mongodb.net/?retryWrites=true&w=majority")
-st.write('OK')
-client = init_connection()
-
-# Pull data from the collection.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def get_data():
-    db = client.mydb
-    items = db.mycollection.find()
-    items = list(items)  # make hashable for st.cache_data
-    return items
-
-items = get_data()
-
-st.write(items)
+st.write('End')
