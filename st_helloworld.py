cc70fa4aeddb6ff4ceda7aba968a527d8bf0a562
Calvin Julo <zcglook@hotmail.com>
2023-07-04 14:13:49 +0800

    Update st_helloworld.py

--- st_helloworld.py
+++ st_helloworld.py
@@ -1,14 +1,17 @@
 # coding=utf-8
 
 import streamlit as st
-import pymongo
-st.write('Start')
-client = pymongo.MongoClient("mongodb+srv://z@cluster0.lfgvy2a.mongodb.net/?retryWrites=true&w=majority")
-st.write('Entry')
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://calvinish:kKS8NAQmdgSqT1c8@cluster0.f8fu9.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     st.write("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     st.write(e)
-
-st.write('End')
