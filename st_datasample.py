88dabe607fb20e7db9caccccd98e4c85b8684a4c
Calvin Julo <cividis@hotmail.com>
2025-02-28 11:12:07 +0800

    Update st_datasample.py

--- st_datasample.py
+++ st_datasample.py
@@ -12,9 +12,30 @@
 
 import pandas as pd
 import streamlit as st
-from coding import input_sample
-from coding import analysis
+# from coding import input_sample
+# from coding import analysis
 
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://mongo00:HgvVqbWtSikGEJKW@cluster0.v53a0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+    st.write('Pinged your deployment. You successfully connected to MongoDB!')
+except Exception as e:
+    print(e)
+
+
+
+'''
 with st.sidebar:
     Username = st.text_input('Username', 'calvinish')
     Password = st.text_input('password', 'kKS8NAQmdgSqT1c8')
@@ -57,4 +78,4 @@ values = []
 for i in data:
     values.append(i[value_name])
 st.dataframe(values)
-
+'''
