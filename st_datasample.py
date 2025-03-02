3be45abb7003642829df9328532e0d172b252e10
Calvin Julo <cividis@hotmail.com>
2025-03-02 19:11:50 +0800

    Update st_datasample.py

--- st_datasample.py
+++ st_datasample.py
@@ -140,18 +140,11 @@ if st.button("Update Database"):
         st.write(update_data)
         # Update the document in MongoDB
         # col_update_one = col.update_one({"_id": doc_id}, {"$set": update_data})
-        col_update_one = col.update({"_id": doc_id}, {"$set": update_data})
+        col_update_one = col.update_one({"_id": doc_id}, {"$set": update_data})
         st.write(f"Row {index} updated; modified count: {col_update_one.modified_count}")
         docs = get_col(db_name, col_name)
 
     st.success("Database updated successfully!")
-
-
-
-
-
-
-
 
 
 
