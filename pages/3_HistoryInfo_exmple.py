a0bed874a6d213e845e20af25fd1f09a720346c5
Calvin Julo <cividis@hotmail.com>
2025-08-03 22:46:30 +0800

    Update 3_HistoryInfo_exmple.py

--- pages/3_HistoryInfo_exmple.py
+++ pages/3_HistoryInfo_exmple.py
@@ -5,7 +5,7 @@
 # File       : xx.py
 # Time       ：2021/9/11 19:02
 # Author     ：
-# version    ：python 3.9
+# version    ：
 # Description：
 """
 # CMD Run Command ： streamlit run /Users/xx.py --server.port 8501
