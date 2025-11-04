2cf62bff42430e3b0d09e9ce400675bdfb8fefb0
Calvin Julo <cividis@hotmail.com>
2025-11-05 03:52:18 +0800

    Update 0_test_example.py

--- pages/0_test_example.py
+++ pages/0_test_example.py
@@ -87,7 +87,8 @@ if len(select_field)==1:
     fig = px.histogram(docs_jn_df, x=select_field, marginal="box", title=f'Distribution of {select_field}')
     st.plotly_chart(fig, use_container_width=True)
 elif len(select_field)==2:
-    fig = px.scatter(docs_jn_df.fillna(''), x=select_field[0], y=select_field[1], title=f'{select_field[0]} vs. {select_field[1]}', trendline="ols")
+    st.write(select_field)
+    fig = px.scatter(docs_jn_df.fillna(''), x=select_field[0], y=select_field[1], title=f'{select_field[0]} vs. {select_field[1]}')
     st.plotly_chart(fig, use_container_width=True)
 elif len(select_field)>2:
     fig = px.scatter_matrix(docs_jn_df.fillna(''),dimensions=select_field,title="Multivariate Relationships")
